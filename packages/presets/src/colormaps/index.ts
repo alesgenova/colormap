4083625a9377d2d0154babc3aa7a94397b40f686
Alessandro Genova <ales.genova@gmail.com>
2019-05-01 21:03:34 -0400

    Rename exported colormap presets to camelcase

--- packages/presets/src/colormaps/index.ts
+++ packages/presets/src/colormaps/index.ts
@@ -1,6 +1,6 @@
-export { default as BLACK_WHITE } from './black-white';
-export { default as CIVIDIS } from './cividis';
-export { default as INFERNO } from './inferno';
-export { default as MAGMA } from './magma';
-export { default as PLASMA } from './plasma';
-export { default as VIRIDIS } from './viridis';
+export { default as blackWhite } from './black-white';
+export { default as cividis } from './cividis';
+export { default as inferno } from './inferno';
+export { default as magma } from './magma';
+export { default as plasma } from './plasma';
+export { default as viridis } from './viridis';
